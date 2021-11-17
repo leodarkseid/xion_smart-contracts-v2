e700502ba895ae0e72735fc01956be4b45c5db28
Xion Developer <dev@xion.finance>
2021-11-17 16:49:31 +0100

    Rename due to collision

--- contracts/rewards/StakingModule_v2.sol
+++ contracts/rewards/StakingModule_v2.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../interfaces/IRewardChest.sol";
 import "../interfaces/IXGTFreezer.sol";
 
-contract StakingModule is
+contract StakingModule_v2 is
     Initializable,
     OwnableUpgradeable,
     PausableUpgradeable
