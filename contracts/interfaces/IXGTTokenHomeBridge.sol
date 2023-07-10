08048bb2d8ce5e0171dd5977ca4002a7037a6e80
Aeryn Quarmby <67911815+aerynquarmby@users.noreply.github.com>
2023-07-10 15:18:43 +0200

    Delete IXGTTokenHomeBridge.sol

--- contracts/interfaces/IXGTTokenHomeBridge.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0
-pragma solidity 0.7.6;
-
-interface IXGTTokenHomeBridge {
-    function outgoingTransfer(uint256 _amount) external;
-
-    function outgoingTransfer(uint256 _amount, address _recipient) external;
-}
